072a0e9573e358b449d246b875a2c4e5548df345
Felix Sidharta Salim <ravenkelder@gmail.com>
2021-03-18 09:49:05 +0000

    Add exit route

---
+++ src/clients/shutdown.ts
@@ -0,0 +1,4 @@
+export async function shutdown(): Promise<number> {
+  return 0;
+}
+
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,23 @@
 import express from "express";
 import tradfriRouter from "./tradfri";
+import { shutdown } from "@/clients/shutdown";
 
 const router = express.Router();
 
 router.use("/tradfri", tradfriRouter)
+
+router.use("/kill", async (req, res) => {
+  var exitCode = 0;
+
+  try {
+    exitCode = await shutdown();  
+  } catch (err) {
+    exitCode = 1;
+  }
+
+  res.status(200).send({ msg: "Exiting with code " + exitCode.toString() });
+
+  process.exit(exitCode);
+});
 
 export default router;
